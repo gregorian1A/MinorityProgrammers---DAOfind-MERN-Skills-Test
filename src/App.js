a3addf463df2da23ac6996351aab67aa02e2e8a6
Gregory Ugbeh <pryscorp@gmail.com>
2021-09-18 01:51:32 +0100

    newer commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { DataProvider } from './context';
 import Sidebar from './components/Sidebar/Sidebar';
 import DaoScreen from './components/DaoScreen/DaoScreen';
